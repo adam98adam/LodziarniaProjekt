985d50e873bf64e986ce5e6a33fa33b2db3bf3ff
adam98adam <adamzebr98@gmail.com>
2020-06-01 13:50:35 +0200

    Polaczenie v1

--- src/projekt/Polaczenie.java
+++ src/projekt/Polaczenie.java
@@ -1,4 +1,68 @@
 package projekt;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Polaczenie {
+
+    private Connection polacz=null;
+    private String baza;
+    private String login;
+    private String haslo;
+
+    public Connection getPolacz() {
+        return polacz;
+    }
+
+    public void setPolacz(Connection polacz) {
+        this.polacz = polacz;
+    }
+
+    public String getBaza() {
+        return baza;
+    }
+
+    public void setBaza(String baza) {
+        this.baza = baza;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login){
+        this.login = login;
+    }
+
+    public String getHaslo() {
+        return haslo;
+    }
+
+    public void setHaslo(String haslo) {
+        this.haslo = haslo;
+    }
+
+    public Connection polaczSie() {
+        try{
+            polacz = DriverManager.getConnection("" +
+                    "databaseName=" + baza  + ";" +
+                    "user=" + login +
+                    ";password=" + haslo + ";");
+            System.out.println("Polaczono !!!");
+        } catch (SQLException throwables) {
+            System.out.println("Blad polaczenia");
+        }
+        return polacz;
+    }
+
+    Polaczenie(){
+        polaczSie();
+    }
+
+    public static void main(String[] args){
+        Polaczenie p = new Polaczenie();
+    }
+
 }
