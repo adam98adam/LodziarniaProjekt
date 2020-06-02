d9348081005e2d8876790bf8d0d300ed5ef35018
adam98adam <adamzebr98@gmail.com>
2020-06-02 22:18:13 +0200

    NapojeZamowienia

---
+++ src/projekt/NapojeZamowienia.java
@@ -0,0 +1,22 @@
+package projekt;
+
+public class NapojeZamowienia {
+    private int idZamowienia,idNapoje;
+
+    public int getIdZamowienia() {
+        return idZamowienia;
+    }
+
+    public void setIdZamowienia(int idZamowienia1) {
+        idZamowienia = idZamowienia1;
+    }
+
+    public int getIdNapoje() {
+        return idNapoje;
+    }
+
+    public void setIdNapoje(int idNapoje1) {
+        idNapoje = idNapoje1;
+    }
+
+}
