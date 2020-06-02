9a02ec9e1ad2265bbe218e709cd463b55dd5fd9e
adam98adam <adamzebr98@gmail.com>
2020-06-02 22:33:26 +0200

    DodatkiLacznik

---
+++ src/projekt/DodatkiLacznik.java
@@ -0,0 +1,22 @@
+package projekt;
+
+public class DodatkiLacznik {
+    private int idLacznik,idDodatki;
+
+    public int getIdLacznik() {
+        return idLacznik;
+    }
+
+    public void setIdLacznik(int idLacznik1) {
+        idLacznik = idLacznik1;
+    }
+
+    public int getIdDodatki() {
+        return idDodatki;
+    }
+
+    public void setIdDodatki(int idDodatki1) {
+        idDodatki = idDodatki1;
+    }
+
+}
