59a9122a0f33271cb4226775372620c4f1eba261
adam98adam <adamzebr98@gmail.com>
2020-06-02 19:36:44 +0200

    Cennik

---
+++ src/projekt/Cennik.java
@@ -0,0 +1,31 @@
+package projekt;
+
+public class Cennik {
+    private int id;
+    private float pojemnosc_ml;
+    private float cena;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id1) {
+        id = id1;
+    }
+
+    public float getPojemnosc_ml() {
+        return pojemnosc_ml;
+    }
+
+    public void setPojemnosc_ml(float pojemnosc_ml1) {
+        pojemnosc_ml = pojemnosc_ml1;
+    }
+
+    public float getCena() {
+        return cena;
+    }
+
+    public void setCena(float cena1) {
+        cena = cena1;
+    }
+}
