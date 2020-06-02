897517e0456667adf55f6d9f41a1d8f614328eab
adam98adam <adamzebr98@gmail.com>
2020-06-02 22:02:24 +0200

    Lacznik v1

---
+++ src/projekt/Lacznik.java
@@ -0,0 +1,37 @@
+package projekt;
+
+public class Lacznik {
+    private int id,idZamowienia,idCennik,idLody;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id1) {
+        id = id1;
+    }
+
+    public int getIdZamowienia() {
+        return idZamowienia;
+    }
+
+    public void setIdZamowienia(int idZamowienia1) {
+        idZamowienia = idZamowienia1;
+    }
+
+    public int getIdCennik() {
+        return idCennik;
+    }
+
+    public void setIdCennik(int idCennik1) {
+        idCennik = idCennik1;
+    }
+
+    public int getIdLody() {
+        return idLody;
+    }
+
+    public void setIdLody(int idLody1) {
+        idLody = idLody1;
+    }
+}
