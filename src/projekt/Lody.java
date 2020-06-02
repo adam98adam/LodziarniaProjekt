9e18fe153e6b8edda70c61821d6a3299ad751b89
adam98adam <adamzebr98@gmail.com>
2020-06-02 18:45:46 +0200

    Lody v1

---
+++ src/projekt/Lody.java
@@ -0,0 +1,31 @@
+package projekt;
+
+public class Lody {
+    private int id;
+    private String smak;
+    private float pojemnosc_L;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id1) {
+        id = id1;
+    }
+
+    public String getSmak() {
+        return smak;
+    }
+
+    public void setSmak(String smak1) {
+        smak = smak1;
+    }
+
+    public float getPojemnosc_L() {
+        return pojemnosc_L;
+    }
+
+    public void setPojemnosc_L(float pojemnosc_L1) {
+        pojemnosc_L = pojemnosc_L1;
+    }
+}
