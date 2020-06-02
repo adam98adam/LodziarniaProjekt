88df538814a08e89e2e40dc624604bc38d6a3458
adam98adam <adamzebr98@gmail.com>
2020-06-02 21:15:32 +0200

    Dodatki v1

---
+++ src/projekt/Dodatki.java
@@ -0,0 +1,32 @@
+package projekt;
+
+public class Dodatki {
+    private  int id;
+    private String nazwa;
+    private float cena;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id1) {
+        id = id1;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa1) {
+        nazwa = nazwa1;
+    }
+
+    public float getCena() {
+        return cena;
+    }
+
+    public void setCena(float cena1) {
+        cena = cena1;
+    }
+
+}
